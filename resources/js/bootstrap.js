90d1f4c2ac6ed87800cd30dd4c06224394b5916d
Elisame Araujo <elisamearaujo@gmail.com>
2024-10-11 18:38:16 -0300

    [atualizado] importado bootstrap e bootstrap icons para o projeto

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,27 @@
+/** === Bootstrap === **/
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
+
+/** PopOvers **/
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+/** Toast **/
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+    toastTrigger.addEventListener('click', () => {
+        toastBootstrap.show()
+    })
+}
+
+/** Tooltip **/
+const tooltipTriggerList = document.querySelectorAll('[data-bs-tooltip="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+/** === Axios === **/
 import axios from 'axios';
 window.axios = axios;
-
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
