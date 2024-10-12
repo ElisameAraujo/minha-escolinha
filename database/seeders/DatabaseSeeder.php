5a69bae810439262b69e6e43729ccecc32326d2b
ElisameAraujo <elisamearaujo@gmail.com>
2024-10-12 18:38:09 -0300

    [atualizado] adicionado seeder novos para categorias dos atletas e fases de um campeonato

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Database\Seeders\Configuracoes\InformacoesProjetoSeeder;
+use Database\Seeders\Campeonatos\FaseCampeonatosSeed;
+use Database\Seeders\Configuracoes\{
+    CategoriasAtletasSeeder,
+    InformacoesProjetoSeeder
+};
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +18,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(InformacoesProjetoSeeder::class);
+        $this->call(CategoriasAtletasSeeder::class);
+        $this->call(FaseCampeonatosSeed::class);
     }
 }
