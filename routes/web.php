816dfa43a03d6a97512b8054cf24ff86a2837122
Elisame Araujo <elisamearaujo@gmail.com>
2024-10-14 10:50:52 -0300

    [atualizado] reorganizado lista de arquivos de rotas

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,10 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+/** == Lista de todas as rotas da aplicação == **/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/** === Autenticação === **/
+require base_path('routes/auth/auth.php');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+/** === Web === **/
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+/** === Administração === **/
