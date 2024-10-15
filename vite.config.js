4ba4a3b946cd1be3c939ecd27874677e911414be
Elisame Araujo <elisamearaujo@gmail.com>
2024-10-15 10:08:13 -0300

    [atualizado] adicionados alias para caminhos genéricos do estilos

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import path from "path";
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
@@ -7,11 +8,17 @@ export default defineConfig({
             input: [
                 'resources/css/app-dashboard.css', 
                 'resources/css/app-web.css', 
-                'resources/css/app.css', 
                 'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            "@bootstrap-icons": path.resolve(__dirname, "./node_modules/bootstrap-icons/"),
+            "@bootstrap": path.resolve(__dirname, "./node_modules/bootstrap/dist/"),
+            "@fonts": path.resolve(__dirname, "./resources/css/fonts/woff2/"),
+        },
+    },
 });
