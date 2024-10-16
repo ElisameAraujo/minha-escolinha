7973d6f516e43de4907b35d225fc5cd3cb29372c
Elisame Araujo <elisamearaujo@gmail.com>
2024-10-16 15:10:15 -0300

    [corrigido] importação da biblioteca js do bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,23 +1,7 @@
 /** === Bootstrap === **/
-import * as bootstrap from 'bootstrap';
+import * as bootstrap from 'bootstrap'
 window.bootstrap = bootstrap;
 
-/** PopOvers **/
-const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
-const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
-
-/** Toast **/
-const toastTrigger = document.getElementById('liveToastBtn')
-const toastLiveExample = document.getElementById('liveToast')
-
-if (toastTrigger) {
-    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-    toastTrigger.addEventListener('click', () => {
-        toastBootstrap.show()
-    })
-}
-
-/** Tooltip **/
 const tooltipTriggerList = document.querySelectorAll('[data-bs-tooltip="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
